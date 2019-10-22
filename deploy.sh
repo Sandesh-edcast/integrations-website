ca782fc4745e3a057b73433cb6f7f18998a5cb8b
Venkatesh Seenivasan <venky@edcast.com>
2019-10-22 11:47:22 -0700

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# aws s3 sync dist/ s3://analytics-events-docs --dryrun
+# aws s3 sync dist/ s3://integrations.edcast.com --dryrun
 echo "Do you want upload files to S3 ?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) aws s3 cp dist s3://analytics-events-docs --recursive;break;;
+        Yes ) aws s3 cp dist s3://integrations.edcast.com --recursive;break;;
         No ) exit;;
     esac
 done
